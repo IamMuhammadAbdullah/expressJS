ec59da2ce708c135db07752bfa26d841a41a8562
IamMuhammadAbdullah <149420366+IamMuhammadAbdullah@users.noreply.github.com>
2025-02-20 22:24:34 +0500

    Update app.js

--- firebaseAuth/src/app.js
+++ firebaseAuth/src/app.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 const app = express();
+const admin = require("firebase-admin");
 
+const serviceAccount = require("../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    allowedHeaders: ["Authorization", "Content-Type"],
+    methods: ["GET", "POST", "PUT", "OPTIONS"],
+  })
+);
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
-app.use('/', authRoutes);
+app.use(express.json());
+app.use("/", authRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
